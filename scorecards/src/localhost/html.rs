928dec04c281970d67001565a12a6c668d4ceb47
Daniel-Anker-Hermansen <daniel201001@gmail.com>
2022-05-10 12:47:35 +0100

    Moved library to its own repos

--- scorecards/src/localhost/html.rs
+++
@@ -1,3 +0,0 @@
-pub fn event_list_to_html(id: &str, rounds: Vec<(String, usize)>) -> String {
-    rounds.iter().map(|(eventid, round)|format!("<a href=round/?eventid={e}&round={r} download={n}{e}{r}.pdf>{e}, {r}</a><br>", r = round, e = eventid, n = id)).collect()
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use scorecards::*;
+use wca_scorecards_lib::*;
 #[allow(unused)]
 use std::env::args;
 
 fn main() {
-    print_subsequent_rounds("dastrupsleepover2022".to_string());
+    print_subsequent_rounds("roskildebynight2022".to_string());
 
     //print_round_1(&mut args().skip(1));
 }
