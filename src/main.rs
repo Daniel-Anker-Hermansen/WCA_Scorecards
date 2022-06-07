38d700f18c280c0542e0923d2689f1c4a9fa4541
Daniel Anker Hermansen <daniel201001@gmail.com>
2022-06-07 21:02:13 +0100

    Added command line arguments

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,23 @@ use wca_scorecards_lib::*;
 use std::env::args;
 
 fn main() {
-    print_subsequent_rounds("roskildebynight2022".to_string());
-
-    //print_round_1(&mut args().skip(1));
+    let mut args = args();
+    let command = args.nth(1);
+    match command {
+        Some(v) if &v == "--r1" => {
+            match (args.next(), args.next(), args.next()) {
+                (Some(group_csv), Some(limit_csv), Some(competition)) => print_round_1_english(&group_csv, &limit_csv, &competition),
+                _ => panic!("Malformatted r1 arguments")
+            }
+            unimplemented!()
+        }
+        Some(v) if &v == "--subseq" => {
+            match args.next() {
+                Some(id) => print_subsequent_rounds(id),
+                None => panic!("No competition id given for subsequent rounds")
+            }
+        }
+        Some(_) => panic!("Invalid command given"),
+        None => panic!("No command given")
+    }
 }
